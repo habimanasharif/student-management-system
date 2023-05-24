4e86cd0ca3cdc9c85c56312d183f8599a71b246f
sharif Habiman <hdidiersharif@gmail.com>
2023-05-24 16:50:57 +0200

    feat:fetching and adding student

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+$_SESSION['student'] =[];
+$st_number=1;
+ $students=[
+  ];
+  if(!empty($_POST["exist_reg_number"])){
+    foreach($_POST["exist_reg_number"] as $key=> $reg_num){
+      $student=[
+        "reg_number"=>$reg_num,
+        "st_name"=>$_POST["exist_st_name"][$key],
+        "st_classroom"=>$_POST["exist_st_classroom"][$key],
+        "st_grade"=>$_POST["exist_st_grade"][$key],
+      ];
+      $students[$key]=$student;
+      $st_number++;
+    }
+  }
+
+ if(isset($_POST)){
+ if(isset($_POST["add_student"])&& $_POST["add_student"]==1){
+  
+  $student=[
+    "reg_number"=>"st_".$st_number,
+    "st_name"=>$_POST["st_name"],
+    "st_classroom"=>$_POST["st_classroom"],
+    "st_grade"=>$_POST["st_grade"],
+  ];
+  $students[]=$student;
+
+ }}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,14 +50,14 @@
         <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title text-center">Add Student</h5>
-    <form action="">
+    <form action="" method="POST">
     <div class="mb-2">
   <label for="exampleFormControlInput1" class="form-label">Student Name</label>
-  <input type="text" class="form-control" id="exampleFormControlInput1" >
+  <input type="text" name="st_name" class="form-control" id="exampleFormControlInput1" >
 </div>
 <div class="mb-2">
   <label for="exampleFormControlInput1" class="form-label">Student Class</label>
-  <select  class="form-select">
+  <select name="st_classroom" class="form-select">
         <option> Class A</option>
         <option> Class B</option>
         <option> Class C</option>
@@ -33,10 +66,23 @@
 </div>
 <div class="mb-2">
   <label for="exampleFormControlInput1" class="form-label">Grade</label>
-  <input type="number" class="form-control" id="exampleFormControlInput1" max="10" min="0">
+  <input type="number" name="st_grade" class="form-control" id="exampleFormControlInput1" max="10" min="0">
 </div>
+<!-- <input type="hidden" name="st_array" value=<?php echo json_encode($students);?>/> -->
+<?php
+ foreach ($students as $student){
+  $st_reg=$student["reg_number"];
+  $st_name=$student["st_name"];
+  $st_classroom=$student["st_classroom"];
+  $st_grade=$student["st_grade"];
+  echo "<input type=\"hidden\" name=\"exist_reg_number[]\" value=\"$st_reg\"/>";
+  echo "<input type=\"hidden\" name=\"exist_st_name[]\" value=\"$st_name\"/>";
+  echo "<input type=\"hidden\" name=\"exist_st_classroom[]\" value=\"$st_classroom\"/>";
+  echo "<input type=\"hidden\" name=\"exist_st_grade[]\" value=\"$st_grade\"/>";
+ }
+?>
 
-<button type="submit" class="btn btn-primary">Add Student</button>
+<button type="submit" name="add_student" value=1 class="btn btn-primary">Add Student</button>
 
     </form>
     
@@ -55,58 +101,20 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    
+    
+    
+    <?php 
+    foreach($students as $student){
+    ?>
+       <tr>
+      <th scope="row"><?php echo explode("_",$student["reg_number"])[1]?></th>
+      <td><?php echo $student["st_name"]?></td>
+      <td><?php echo $student["st_classroom"]?></td>
+      <td><?php echo $student["st_grade"]?></td>
       <td>
       <div class="dropdown">
-  <button class="btn " type="button" id="ellipsisDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  <div class="d-flex flex-column">
-         <div class="dot"></div>
-         <div class="dot"></div>
-         <div class="dot"></div>
-      </div>
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="ellipsisDropdown">
-    <li><a class="dropdown-item" href="#">Edit</a></li>
-    <li><a class="dropdown-item" href="#">Done</a></li>
-    <li><a class="dropdown-item" href="#">Delete</a></li>
-  </ul>
-</div>
-    </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>
-      <div class="dropdown">
-  <button class="btn " type="button" id="ellipsisDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  <div class="d-flex flex-column">
-         <div class="dot"></div>
-         <div class="dot"></div>
-         <div class="dot"></div>
-      </div>
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="ellipsisDropdown">
-    <li><a class="dropdown-item" href="#">Edit</a></li>
-    <li><a class="dropdown-item" href="#">Done</a></li>
-    <li><a class="dropdown-item" href="#">Delete</a></li>
-  </ul>
-</div>
-        </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>
-      <div class="dropdown">
-  <button class="btn " type="button" id="ellipsisDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <button class="btn "  type="button" id="ellipsisDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
   <div class="d-flex flex-column">
          <div class="dot"></div>
          <div class="dot"></div>
@@ -115,22 +123,30 @@
   </button>
   <ul class="dropdown-menu" aria-labelledby="ellipsisDropdown">
     <li><a class="dropdown-item" href="#"><!-- Button trigger modal -->
-<button type="button" class="btn w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <?php 
+      $array_st="#".str_replace(' ','-',$student["st_name"]);
+      echo "<button type='button' name='st_action' value='$array_st' class='btn w-100' data-bs-toggle='modal' data-bs-target='$array_st'> "
+      ?>
   Edit
 </button>
 </a></li>
 
-    <li><a class="dropdown-item" href="#">Delete</a></li>
+    <li><a class="dropdown-item" href="#">
+      <form action="" method="POST">
+      <button>Delete</button>
+      </form>
+    </a></li>
   </ul>
 </div>    
     </td>
     </tr>
-    
+     <?php }?>   
   </tbody>
 </table>
 
+<?php  foreach($students as $student){?>
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id=<?php echo  str_replace(' ','-',$student["st_name"])?> tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -140,21 +156,22 @@
       <div class="modal-body">
       <form action="">
     <div class="mb-2">
-  <label for="exampleFormControlInput1" class="form-label">Student Name</label>
-  <input type="text" value="Mark Otto" class="form-control" id="exampleFormControlInput1" >
+     <label for='exampleFormControlInput1' class='form-label'>Student Name</label>
+  <input type='text' value="<?php echo htmlspecialchars($student["st_name"])?>" class='form-control' id='exampleFormControlInput1' >
+
 </div>
 <div class="mb-2">
   <label for="exampleFormControlInput1" class="form-label">Student Class</label>
-  <select  class="form-select">
-        <option> Class A</option>
-        <option selected> Class B</option>
-        <option> Class C</option>
-        <option> Class D</option>
+      <select  class="form-select">
+        <option <?php if($student['st_classroom']== 'Class A'){echo "selected";} ?>  > Class A</option>
+        <option <?php if($student['st_classroom']== 'Class B'){echo "selected";} ?> > Class B </option>
+        <option <?php if($student['st_classroom']== 'Class C'){echo "selected";} ?>  > Class C</option>
+        <option <?php  if($student['st_classroom']== 'Class D'){echo "selected";} ?> > Class D</option>
       </select>
 </div>
 <div class="mb-2">
   <label for="exampleFormControlInput1" class="form-label">Grade</label>
-  <input type="number" value="10" class="form-control" id="exampleFormControlInput1" max="10" min="0">
+  <input type="number" value=<?php echo $student["st_grade"] ?> class="form-control" id="exampleFormControlInput1" max="10" min="0">
 </div>
 
 <button type="submit" class="btn btn-primary">Update Student</button>
@@ -164,6 +181,7 @@
     </div>
   </div>
 </div>
+<?php } ?>
         </div>
         </div>
     </div>
